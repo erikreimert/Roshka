5ced794926e7449b68410a968a55232131c72c1a
erikreimert <erikreimert0@gmail.com>
2020-06-30 12:08:52 -0400

    fixed an oopsie

--- Excel Changer/downloadbot2.py
+++ Excel Changer/downloadbot2.py
@@ -5,30 +5,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+###############################################
+#Test file with github
+##############################################
+def bot(user, pword):
 
-user = 'erikreimert'
-pword = 'Alemania1968$'
+    browser = webdriver.Chrome()
+    browser.get(('https://github.com/login'))
 
-browser = webdriver.Chrome()
-browser.get(('https://github.com/login'))
+    #TODO
+    username = browser.find_element_by_id('login_field')
+    username.send_keys(user)
+    username = browser.find_element_by_id('password')
+    username.send_keys(pword)
+    nextButton = browser.find_element_by_name('commit')
+    nextButton.click()
 
-#TODO
-username = browser.find_element_by_id('login_field')
-username.send_keys(user)
-username = browser.find_element_by_id('password')
-username.send_keys(pword)
-nextButton = browser.find_element_by_name('commit')
-nextButton.click()
+    Esperar = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.XPATH,"//span[@title='Machine-Learning']")))
+    Esperar.click()
 
-Esperar = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.XPATH,"//span[@title='Machine-Learning']")))
-Esperar.click()
+    Esperar = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.XPATH,"//summary[@class='btn  ml-2 btn-primary']")))
+    Esperar.click()
 
-Esperar = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.XPATH,"//summary[@class='btn  ml-2 btn-primary']")))
-Esperar.click()
+    descargar = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.LINK_TEXT,'Download ZIP')))
+    descargar.click()
 
-descargar = WebDriverWait(browser, 1).until(EC.element_to_be_clickable((By.LINK_TEXT,'Download ZIP')))
-descargar.click()
+    time.sleep(20)
 
-time.sleep(15)
+    os.system('python mover.py')
 
-os.system('python mover.py')
+if __name__ == "__main__":
+     user = input("Enter username: ")
+     pwor = input("Enter password: ")
+     bot(user, pword)
