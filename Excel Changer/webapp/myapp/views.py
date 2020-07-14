d9245d1496dc7576ec657896e4ce22a7a887cf24
erikreimert <erikreimert0@gmail.com>
2020-07-14 13:54:48 -0400

    django es mi puta

--- Excel Changer/webapp/myapp/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def upload(request):
-    return render(request. 'gui.html')
--- Excel Changer/webapp/webapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from web import views
-
-    urlpatterns = [
-        path('consolidacion/', views.upload, name='upload'),
-    ]
