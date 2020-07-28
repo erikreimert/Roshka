84fac90869d0efb140fb04b45831f387b067df15
Erik <erikreimert0@gmail.com>
2020-07-28 14:27:41 -0400

    fixed typo

--- Consolidator/mysite/myapp/program/folders.py
+++ Consolidator/mysite/myapp/program/folders.py
@@ -1,7 +1,7 @@
 #los address, mas facil editar una vez aca que mil veces en el archivo
 
 data = '~/Roshka/Consolidator/mysite/statics/data/'
-BrosCo_Si_Bancop_No = '~//Roshka/Consolidator/mysite/statics/BrosCo_Si_Bancop_No/'
+BrosCo_Si_Bancop_No = '~/Roshka/Consolidator/mysite/statics/BrosCo_Si_Bancop_No/'
 BrosCo_Original = '~/Roshka/Consolidator/mysite/statics/BrosCo_Original/'
 Bancop_Original = '~/Roshka/Consolidator/mysite/statics/Bancop_Original/'
 dl = '~/Downloads/'
